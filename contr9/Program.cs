5c8d8fd26416287d2e03b646e5e47f296b30d6e9
Imanbekov Tologon <imanbekovtologon4@gmail.com>
2024-06-22 19:31:10 +0600

    wrote spent time to timeWorked.txt

--- contr9/Program.cs
+++ contr9/Program.cs
@@ -46,6 +46,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Transaction}/{action=Create}/{id?}");
 
 app.Run();
